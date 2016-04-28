559777e2befb9e3b1f3c2913b7281a04430eb08e
ojSeyi <1509941@rgu.ac.uk>
2016-04-28 03:17:13 +0100

    1st commit

--- superheroes.php
+++ superheroes.php
@@ -5,7 +5,7 @@
     <title>Marvel Heroes</title>
 </head>
 <body>
-    <ul>
+
     <?php
         if(empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['gender']) || empty($_POST['power'])){
             echo "Please enter all details of Super Hero";
@@ -17,10 +17,10 @@
             $power = $_POST['power'];
 
             echo
-                "{<li>{$firstname} {$lastname} {$gender} {power} </li>}";
+                "<h2>{$firstname} {$lastname} {$gender} {$power}</h2>";
 
         }
     ?>
-    </ul>
+
 </body>
 </html>
