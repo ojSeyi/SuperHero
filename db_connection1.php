4e43e66ed6abecfca3038308c171956396a2bf30
ojSeyi <1509941@rgu.ac.uk>
2016-04-28 05:56:17 +0100

    2nt commit

--- db_connection1.php
+++ db_connection1.php
@@ -1,10 +1,10 @@
 <?php
     define('host', 'us-cdbr-azure-southcentral-e.cloudapp.net")');
-    define('username', 'be3149d439cf7b');
-    define('password', '7fe76a1d');
+    define('username', 'bbf3c48901b85f');
+    define('password', '8038ddc6');
     define('database', 'marvelheroes');
 
-$db = mysqli_connect("host", "username", "password", "database");
+$db = mysqli_connect(host, username, password, database);
 
 if(mysqli_connect_errno()){
     die('Connectfailed['.mysqli_connect_error($db).']');
