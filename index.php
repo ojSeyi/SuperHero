c93d11f284582905c4a108ffd69267c6f03fe1ab
ojSeyi <1509941@rgu.ac.uk>
2016-04-28 03:08:57 +0100

    1st commit

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Marvel Heroes</title>
 </head>
 <body>
-
+    <div class="heroreg">
+        <form action="superheroes.php" method="post">
+            <label>Super Hero Registration</label>
+            <input type="text" placeholder="First Name" name="firstname">
+            <input type="text" placeholder="Last Name" name="lastname">
+            <input type="radio" value="male" name="gender">
+            <input type="radio" value="female" name="gender">
+            <input type="text" placeholder="Main Superhero Power" name="power">
+        </form>
+    </div>
 </body>
 </html>
--- superheroes.php
+++ superheroes.php
@@ -2,9 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Marvel Heroes</title>
 </head>
 <body>
+    <ul>
+    <?php
+        if(empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['gender']) || empty($_POST['power'])){
+            echo "Please enter all details of Super Hero";
+        }
+        else{
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $gender = $_POST['gender']; 
+            $power = $_POST['power'];
 
+            echo
+                "{<li>{$firstname} {$lastname} {$gender} {power} </li>}";
+
+        }
+    ?>
+    </ul>
 </body>
 </html>
