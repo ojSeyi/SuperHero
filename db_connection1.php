188c4fd5651b5a5b15e7793ccf911628cbb865e2
ojSeyi <1509941@rgu.ac.uk>
2016-04-28 17:33:50 +0100

    2nt commit

--- db_connection1.php
+++ db_connection1.php
@@ -4,7 +4,6 @@ define('DB_USERNAME', 'be3149d439cf7b');
 define('DB_PASSWORD', '7fe76a1d');
 define('DB_NAME', 'DB1509941_1');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-?>
 
 if(mysqli_connect_errno($db)){
     echo "Failed to connect: " . mysqli_error();
