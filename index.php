5a0ac9e30093b6b24082f33ee1d2c94fd4166e78
ojSeyi <1509941@rgu.ac.uk>
2016-04-28 03:15:02 +0100

    1st commit

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
     <div class="heroreg">
         <form action="superheroes.php" method="post">
             <label>First Name </label>
-            <input type="text" placeholder="First Name" name="firstname"><br>
+            <input type="text" placeholder="First Name" name="firstname"><br><br>
             <label>Last Name </label>
-            <input type="text" placeholder="Last Name" name="lastname"><br>
-            <label>Gender </label>
-            <input type="radio" value="male" name="gender"><br>
-            <input type="radio" value="female" name="gender"><br>
+            <input type="text" placeholder="Last Name" name="lastname"><br><br>
+            <label>Male </label>
+            <input type="radio" value="male" name="gender"><br><br>
+            <label>Female </label>
+            <input type="radio" value="female" name="gender"><br><br>
             <label>Power </label>
-            <input type="text" placeholder="Main Superhero Power" name="power"><br>
+            <input type="text" placeholder="Main Superhero Power" name="power"><br><br>
             <input type="submit" value="Go"><br>
         </form>
     </div>
